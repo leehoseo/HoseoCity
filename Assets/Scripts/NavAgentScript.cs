ab22cb62fd84fe4eb3e87a016a865aec7f238a8f
leehoseo <leehoseo@game.hoseo.edu>
2016-12-28 13:05:02 +0900

    NavMeshTest

---
+++ Assets/Scripts/NavAgentScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavAgentScript : MonoBehaviour {
+
+    [SerializeField]
+    Transform m_target;
+
+    NavMeshAgent m_agent;
+	// Use this for initialization
+	void Start () {
+        m_agent = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_agent.SetDestination(m_target.position);
+	}
+
+    public Transform target
+    {
+        get { return m_target; }
+        set { m_target = value; }
+    }
+}
